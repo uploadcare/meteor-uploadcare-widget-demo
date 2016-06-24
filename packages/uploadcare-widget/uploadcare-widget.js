9cc2fcedbf36207954f541f38e6f1b39ccf41324
Zarema Khalilova <zarema.khalilova@gmail.com>
2016-06-24 16:53:03 +0300

    Create package uploadcare:uploadcare-widget

---
+++ packages/uploadcare-widget/uploadcare-widget.js
@@ -0,0 +1,5 @@
+// Write your package code here!
+
+// Variables exported by this module can be imported by other packages and
+// applications. See uploadcare-widget-tests.js for an example of importing.
+export const name = 'uploadcare-widget';
