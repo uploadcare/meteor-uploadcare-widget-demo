3a85a080a140f83a1cb6176463bac8e649e49aa6
Zarema Khalilova <zarema.khalilova@gmail.com>
2016-08-05 14:47:05 +0300

    Remove uploadcare-widget from packages

--- packages/uploadcare-widget/uploadcare-widget.js
+++
@@ -1,9 +0,0 @@
-import uploadcare from 'uploadcare-widget';
-
-const meteorUploadcareSettings = ((Meteor.settings || {}).public || {}).uploadcare || {};
-
-if(!!meteorUploadcareSettings) {
-  uploadcare.start(meteorUploadcareSettings);
-}
-
-export default uploadcare;
