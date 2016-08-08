d758a8bb3fa086c665ec97239e10c8db05e253df
Zarema Khalilova <zarema.khalilova@gmail.com>
2016-08-08 16:14:46 +0300

    Reformate js code

--- client/main.js
+++ client/main.js
@@ -1,29 +1,30 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
-import uploadcare from 'meteor/uploadcare:uploadcare-widget';
+import {Template} from 'meteor/templating'
+import {ReactiveVar} from 'meteor/reactive-var'
+import uploadcare from 'meteor/uploadcare:uploadcare-widget'
 
-import './main.html';
+import './main.html'
 
 Template.show.onRendered(function() {
-  let widget = uploadcare.Widget('#file-show');
+	let widget = uploadcare.Widget('#file-show')
 
-  widget.onUploadComplete(info => {
-    // Handle uploaded file info.
-    this.uuid.set(info.uuid)
-    this.cdnUrl.set(info.cdnUrl)
-  });
-});
+	widget.onUploadComplete(info => {
+		// Handle uploaded file info.
+		this.uuid.set(info.uuid)
+		this.cdnUrl.set(info.cdnUrl)
+	})
+})
 
 Template.show.onCreated(function() {
-  this.uuid = new ReactiveVar('');
-  this.cdnUrl = new ReactiveVar('');
-});
+	this.uuid = new ReactiveVar('')
+	this.cdnUrl = new ReactiveVar('')
+})
 
 Template.show.helpers({
-  uuid() {
-    return Template.instance().uuid.get();
-  },
-  cdnUrl() {
-    return Template.instance().cdnUrl.get();
-  },
-});
+	uuid() {
+		return Template.instance().uuid.get()
+	},
+	cdnUrl() {
+		return Template.instance().cdnUrl.get()
+	},
+})
+
